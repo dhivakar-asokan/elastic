21c4c2510a7f05ec0855b4402b0f64a9a2427d7c
Dhivakar Asokan <48990091+dhivakar-asokan@users.noreply.github.com>
2022-11-25 15:42:35 +0530

    Create engine.sh

---
+++ scripts/engine.sh
@@ -0,0 +1,3 @@
+curl --location --request GET 'https://daso.apigw-aw-eu.webmethods.io/rest/apigateway/health/engine' \
+--header 'Accept: application/json' \
+--header 'Authorization: Basic ZGFzbzpXZXN0QDEyMzQ='https://github.com/dhivakar-asokan/elastic
