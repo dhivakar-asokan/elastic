238ee6aec4cc1f21c0a40d845c70c5333afb10b5
Dhivakar Asokan <48990091+dhivakar-asokan@users.noreply.github.com>
2022-11-25 15:42:10 +0530

    Create admin.sh

---
+++ scripts/admin.sh
@@ -0,0 +1,3 @@
+curl --location --request GET 'https://daso.apigw-aw-eu.webmethods.io/rest/apigateway/health/admin' \
+--header 'Accept: application/json' \
+--header 'Authorization: Basic ZGFzbzpXZXN0QDEyMzQ='
