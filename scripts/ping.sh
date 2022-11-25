1b00913cae638c5027b3cae74f86c45826e6774f
Dhivakar Asokan <48990091+dhivakar-asokan@users.noreply.github.com>
2022-11-25 14:58:00 +0530

    Create ping.sh

---
+++ scripts/ping.sh
@@ -0,0 +1,3 @@
+curl --location --request GET 'https://daso.apigw-aw-eu.webmethods.io/rest/apigateway/apis/dd28d3c5-122c-43b2-b088-dd4a5ffb75cb' \
+--header 'Accept: application/json' \
+--header 'Authorization: Basic ZGFzbzpXZXN0QDEyMzQ='
